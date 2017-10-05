84cf7ebde61772664d3c21853c3e2f8cb77bd33c
Andreas Möller <am@localheinz.com>
2017-10-05 12:51:07 +0200

    Fix: Typo

--- test/Unit/ConstructsTest.php
+++ test/Unit/ConstructsTest.php
@@ -46,7 +46,7 @@ final class ConstructsTest extends Framework\TestCase
         }
     }
 
-    public function testFromDirectoryTHrowsDirectoryDoesNotExistIfDirectoryDoesNotExist()
+    public function testFromDirectoryThrowsDirectoryDoesNotExistIfDirectoryDoesNotExist()
     {
         $directory = __DIR__ . '/NonExistent';
 
