5682b53ba488dc28849fb305318e4c543e5f0a8d
Andreas Möller <am@localheinz.com>
2025-12-24 15:30:51 +0100

    Fix: Add test

---
+++ test/Unit/Exception/SourceCouldNotBeParsedTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/classy
+ */
+
+namespace Ergebnis\Classy\Test\Unit\Exception;
+
+use Ergebnis\Classy\Exception;
+use Ergebnis\Classy\FilePath;
+use Ergebnis\Classy\Test;
+use PHPUnit\Framework;
+
+/**
+ * @covers \Ergebnis\Classy\Exception\SourceCouldNotBeParsed
+ *
+ * @uses \Ergebnis\Classy\FilePath
+ */
+final class SourceCouldNotBeParsedTest extends Framework\TestCase
+{
+    use Test\Util\Helper;
+
+    public function testFromParseErrorReturnsException(): void
+    {
+        $parseError = new \ParseError(self::faker()->sentence());
+
+        $exception = Exception\SourceCouldNotBeParsed::fromParseError($parseError);
+
+        self::assertSame('Source could not be parsed.', $exception->getMessage());
+        self::assertSame($parseError, $exception->getPrevious());
+    }
+}
