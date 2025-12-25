bc8870a7c14617610357af7e6fb55fbe89b508ce
Andreas Möller <am@localheinz.com>
2025-12-25 15:02:49 +0100

    Fix: Add test

---
+++ test/Unit/Exception/InvalidSourceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/classy
+ */
+
+namespace Ergebnis\Classy\Test\Unit\Exception;
+
+use Ergebnis\Classy\Exception;
+use PHPUnit\Framework;
+
+/**
+ * @covers \Ergebnis\Classy\Exception\InvalidSource
+ */
+final class InvalidSourceTest extends Framework\TestCase
+{
+    public function testBlankOrEmptyReturnsException(): void
+    {
+        $exception = Exception\InvalidSource::blankOrEmpty();
+
+        self::assertSame('Source can not be blank or empty.', $exception->getMessage());
+    }
+}
