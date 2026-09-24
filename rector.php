217d541dc707b3fe533d10ded527865f317045b9
Andreas Möller <am@localheinz.com>
2026-09-24 16:20:46 +0200

    Enhancement: Enable and configure ReferenceNamespacedSymbolsRelativeToNamespacePrefixRector

--- rector.php
+++ rector.php
@@ -33,6 +33,14 @@ return static function (Config\RectorConfig $rectorConfig): void {
         Rector\Rules\Faker\GeneratorPropertyFetchToMethodCallRector::class,
     ]);
 
+    $rectorConfig->ruleWithConfiguration(Rector\Rules\Files\ReferenceNamespacedSymbolsRelativeToNamespacePrefixRector::class, [
+        'discoverNamespacePrefixes' => true,
+        'parentNamespacePrefixes' => [
+            'Ergebnis\Classy',
+            'Symfony\Component',
+        ],
+    ]);
+
     $rectorConfig->sets([
         PHPUnit\Set\PHPUnitSetList::COMPOSER_BASED,
     ]);
